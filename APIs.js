b7100cefdc335ebab6c2d7e1bb8850068f689baf
Karla Rangel <krangelhdz@gmail.com>
2025-01-15 16:58:08 +0000

    file re APIs

--- APIs.js
+++ APIs.js
@@ -7,5 +7,17 @@
 // The term "API" refers to any external server that can be ineracted with through HTTP requests -- When we create these HTTP
 // requests, we are " calling an API".
 
-// Ex: to interact with the Github API to do a GET request of a repo, we need the command-line tool "curl" + URL -->
-// curl https://api.github.com/repos/evanw/esbuild
+// Ex:
+//  - To interact with the Github API to do a GET request of a repo, we need the command-line tool "curl" + URL -->
+//    curl https://api.github.com/repos/evanw/esbuild
+
+//  - Another way of performing GET requests is simply to visit the URL address in your browser.
+
+// The above are 2 methods of creating HTTP requests to interact with an API.
+
+// The real power is when write code that can interact with an API -- sending HTTP requests directly from our code. Enter "fetch"!!
+
+// "fetch" is a funtion built into JS for creating HTTP requests. We pass a URL to "fetch" & it'll perform a GET request to that URL:
+//    fetch('https://api.github.com/repos/evanw/esbuild')
+
+// "fetch" is asynchronous, it takes time to complete, so it's attached to a promise.
