00114a875a14500aad3897e52b97c52ccd96c5c5
Karla Rangel <krangelhdz@gmail.com>
2025-01-15 16:57:35 +0000

    file re APIs

---
+++ APIs.js
@@ -0,0 +1,11 @@
+// API: Application Programming Interface
+// It is how we interact with a program using code. When the program runs on an external server,
+// we interact with it through HTTP requests, sent via the internet.
+
+// For applications running on a server, the API is the definition of what HTTP requests the server will respond to, and the format of the response it will send back.
+
+// The term "API" refers to any external server that can be ineracted with through HTTP requests -- When we create these HTTP
+// requests, we are " calling an API".
+
+// Ex: to interact with the Github API to do a GET request of a repo, we need the command-line tool "curl" + URL -->
+// curl https://api.github.com/repos/evanw/esbuild
